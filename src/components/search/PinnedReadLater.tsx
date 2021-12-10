ab3882f2b2c23439be8aad2f6e0f2aa2310ddc63
andots <18536+andots@users.noreply.github.com>
2021-12-11 01:53:37 +0900

    [feat] Remove Pinned Read Later by close button

--- src/components/search/PinnedReadLater.tsx
+++ src/components/search/PinnedReadLater.tsx
@@ -5,6 +5,9 @@ import { IconButton } from '@mui/material';
 import FlexBox from 'src/components/atoms/FlexBox';
 import TypographyText from 'src/components/atoms/TypographyText';
 import SimpleItem from 'src/components/search/SimpleItem';
+import { useUpdateBookmarkMutation } from 'src/redux/services/elasticsearch/api';
+import { updateSearchHit } from 'src/redux/slices/searchSlice';
+import { useAppDispatch } from 'src/redux/store';
 import type { BookmarkResponse } from 'src/types';
 
 type Props = {
@@ -12,9 +15,22 @@ type Props = {
 };
 
 function PinnedReadLater({ hits }: Props): JSX.Element {
+  const dispatch = useAppDispatch();
+  const [updateBookmark, {}] = useUpdateBookmarkMutation();
+
   if (hits.length == 0) {
     return <></>;
   }
+
+  const handleRemove = (item: SearchHit<BookmarkResponse>) => {
+    const { _id, _index } = item;
+    updateBookmark({
+      id: _id,
+      index: _index,
+      body: { doc: { isReadLater: false } },
+    });
+    dispatch(updateSearchHit({ id: _id, index: _index, patch: { isReadLater: false } }));
+  };
 
   return (
     <FlexBox flexDirection="column" sx={{ mb: 3 }}>
@@ -27,7 +43,10 @@ function PinnedReadLater({ hits }: Props): JSX.Element {
         if (item._source != null) {
           return (
             <FlexBox sx={{ position: 'relative', left: '-6px' }}>
-              <IconButton size="small" sx={{ marginRight: 1 }}>
+              <IconButton
+                onClick={() => handleRemove(item)}
+                size="small"
+                sx={{ marginRight: 1 }}>
                 <Close />
               </IconButton>
               <SimpleItem searchHit={item} />
